866dbb7ddd5075c651f8a8a75318b99f1a7e7ba2
Rebecca Fribourg <rebecca.fribourg@hotmail.fr>
2016-04-13 17:25:42 +0200

    lauch eclairer activity from menu

--- Agir/app/src/main/java/com/tx/agir/Adapter.java
+++ Agir/app/src/main/java/com/tx/agir/Adapter.java
@@ -1,9 +1,14 @@
 package com.tx.agir;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Rebecca on 13/04/2016.
  */
-public class Adapter extends AppCompatActivity {
+public class Adapter extends Activity {
+
+
+
+
 }
--- Agir/app/src/main/java/com/tx/agir/Eclairer.java
+++ Agir/app/src/main/java/com/tx/agir/Eclairer.java
@@ -1,9 +1,23 @@
 package com.tx.agir;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by Rebecca on 13/04/2016.
  */
-public class Eclairer extends AppCompatActivity {
+public class Eclairer extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eclairer_layout);
+
+    }
+
+
 }
